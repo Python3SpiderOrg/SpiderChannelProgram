1a0a317a85323488e0f0c1df4de72023c1561a60
liumingjun <405702878@qq.com>
2018-10-06 00:15:03 +0800

    加上判断爬取的节目列表是否为空

--- spider.py
+++ spider.py
@@ -28,11 +28,11 @@ def read_channelname():
     try:
         with open('channelname.txt','r') as f:
             for line in f.readlines():
-                if line=='HNTV':
+                if line.strip('\n')=='HNTV':
                     line = 'HUNANTV1'
-                elif line=='GXTV':
-                    line = 'GUANXITV1'
-                elif line=='CCTV15':
+                elif line.strip('\n')=='GXTV':
+                    line = 'GUANXI1'
+                elif line.strip('\n')=='CCTV15':
                     line = 'CCTV16'
                 elif line.strip('\n') in cctv_prog:
                     line = line
@@ -50,32 +50,41 @@ def read_channelname():
 def get_program_info(channelname):
     now = datetime.datetime.now()
     this_week_start = now - timedelta(days=now.weekday()) #获取本周一的时间
+    driver_path = os.getcwd() + '\driver\phantomjs.exe'
+    driver = webdriver.PhantomJS(executable_path=driver_path)
     for name in channelname:
-        for i in range(1,3):
+        print("正在爬取%s的节目单!" % name)
+        if os.path.exists(os.getcwd() + "\output\%s.txt" % name):
+            os.remove(os.getcwd() + "\output\%s.txt" % name)
+        for i in range(1,15):
+#            print("正在爬取%s的第%d天节目单!" % (name,i))
             if name in cctv_prog:
                 url = link+'/program/'+'CCTV-'+name+'-w'+str(i)+'.html'
             elif name in province_prog:
                 url = link+'/program_satellite/'+name+'-w'+str(i)+'.html'
             else:
                 raise ("频道名称错误，请检查！")
-            driver_path=os.getcwd()+'\driver\chromedriver.exe'
-            driver =  webdriver.Chrome(executable_path=driver_path)
+#            print(url)
             driver.get(url)
             page_source = driver.page_source
-            driver.quit()
             soup = BeautifulSoup(page_source,'lxml')
-            program_data = soup.find_all(class_="over_hide")
-            with open("%s.txt" % name, "a") as f:
-                f.writelines((this_week_start + datetime.timedelta(days=(i - 1))).strftime("%Y/%m/%d"))
-                f.write('\n')
-            for program in program_data:
-                program_time = program.find("span").string
-                program_name = program.find(class_="p_show").text
-                with open("%s.txt"%name,"a") as f:
-                    f.writelines(program_time+'  '+program_name)
+            program_data = soup.find(id="pgrow")
+#            print(program_data)
+            if program_data.find("span") is not None:
+                with open(os.getcwd()+"\output\%s.txt" % name, "a") as f:
+                    f.writelines((this_week_start + datetime.timedelta(days=(i - 1))).strftime("%Y/%m/%d"))
                     f.write('\n')
-            time.sleep(1)
+                for program in program_data.find_all('li'):
+                    if  program.get('id') not in ('noon','night') :
+                        program_time = program.find("span").text
+    #                    print(program_time)
+                        program_name = program.find(class_="p_show").text
+                        with open(os.getcwd()+"\output\%s.txt"%name,"a") as f:
+                            f.writelines(program_time+'  '+program_name)
+                            f.write('\n')
+    driver.quit()
 
 if __name__=='__main__':
     name = read_channelname()
     get_program_info(name)
+    print("所有频道的节目单爬取完毕！")
